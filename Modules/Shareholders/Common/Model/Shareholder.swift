ea0dfbe36c001ce7452a3fe7f86f665dc87a17c2
Maker <s.shalgin@gmail.com>
2023-02-05 19:27:51 +0300

    add Shareholder model

---
+++ Modules/Shareholders/Common/Model/Shareholder.swift
@@ -0,0 +1,12 @@
+// Created by Станислав on 05.02.2023.
+
+import SharedProtocolsAndModels
+
+struct Shareholder: Decodable {
+    let id: String
+    let iconURL: String
+    let name: String
+    let company: String
+    let amount: Amount
+    let profit: Float
+}
