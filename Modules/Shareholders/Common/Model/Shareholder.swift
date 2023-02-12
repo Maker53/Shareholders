3b3f597f7ae5214129f0dd9fa1f9b09aac0c2aad
Maker <s.shalgin@gmail.com>
2023-02-12 16:50:54 +0300

    refactor model

--- Modules/Shareholders/Common/Model/Shareholder.swift
+++ Modules/Shareholders/Common/Model/Shareholder.swift
@@ -2,11 +2,20 @@
 
 import SharedProtocolsAndModels
 
-struct Shareholder: Decodable {
+struct Shareholder: Decodable, Equatable {
     let id: String
     let iconURL: String
     let name: String
-    let company: String
+    let company: Company
     let amount: Amount
     let profit: Double
+}
+
+enum Company: String, Decodable {
+    case Tinek, Alfabank, Sber
+    case unknown
+    
+    init(from decoder: Decoder) throws {
+        self = try Self(rawValue: decoder.singleValueContainer().decode(RawValue.self)) ?? .unknown
+    }
 }
